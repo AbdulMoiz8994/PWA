f6836ee0f5309b311d4e592a477edd12f6f060f6
AbdulMoiz8994 <moiza8994@gmail.com>
2021-04-26 14:45:51 +0500

    we are just doing practice of pwa apps serviceWorer and minesfest

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('service-worker.js').then(function(data) {
-        console.log('navigator ,' ,data);
+        console.log('navigator ' ,data);
     }).catch(function(err){
            console.log("Some thing went wrong with service workder");
            console.log(err);
--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,32 @@
-var cacheName= "demo-app";
+var cacheName = "demo-app";
 
-var fileTOCache=[
-    '/',
-    '/index.js',
-    '/index.html',
-    '/About.html'
+// addAll takes an array that's why are making in  array []
+var fileTOCache = [
+  "/",
+  "/index.js",
+  "/index.html",
+  // '/About.html'  
+];
 
-]
-
-
-self.addEventListener('activate',(e) =>{
-    console.log("sericeWorker Activated");
+self.addEventListener("activate", (e) => {
+  console.log("sericeWorker Activated");
 });
 
-self.addEventListener('install',(e) =>{
-   console.log("Service Workder insatll");
-   e.waitUntil(
-       caches.open(cacheName).then(function(cache){
-            console.log("serviceWorker we are saving our file in cache");
-          return  cache.addAll(fileTOCache)
-       })
-   )
+self.addEventListener("install", (e) => {
+  console.log("Service Workder insatll");
+  e.waitUntil(
+    caches.open(cacheName).then(function (data) {
+      console.log("serviceWorker we are saving our file in cache");
+      return data.addAll(fileTOCache);
+    })
+  );
 });
 
-self.addEventListener('fetch',(e) =>{
-    console.log("serviceWorker Fetch",e.request.url);
-    e.respondWith(
-        caches.match(e.request).then((response) =>{
-            return response || fetch(e.request)
-        })
-    )
-})
+self.addEventListener("fetch", (e) => {
+  console.log("serviceWorker Fetch", e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
